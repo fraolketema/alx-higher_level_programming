1aebe230f08796bdde5a9b3b905af154b5812855
Firaol Ketema <ketemafiraol94@gmail.com>
2023-11-28 11:59:24 -0800

    Print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
+
 def print_last_digit(number):
-    if number < 0:
-        last_num = (-number % 10)
-    elif number >= 0:
-        last_num = number % 10
-    print("{:d}".format(last_num), end="")
-        return last_num
+    ls = abs(number) % 10
+    print(ls, end="")
+    return ls
