048433d1bdcc2c90b30827a484bb161ec7a3f3c2
Firaol Ketema <ketemafiraol94@gmail.com>
2023-11-28 12:09:01 -0800

    fixing errors

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
+
 import random
+number = random.randint(-10, 10)
 
-def classify_number(number):
-    if number > 0:
-        return f"{number} is positive"
-    elif number == 0:
-        return f"{number} is zero"
-    else:
-        return f"{number} is negative"
-
-def main():
-    number = random.randint(-10, 10)
-    result = classify_number(number)
-    print(result)
-
-if __name__ == "__main__":
-    main()
+if number > 0:
+    print(f'{number} is positive')
+elif number == 0:
+    print(f'{number} is zero')
+else:
+    print(f'{number} is negative')
